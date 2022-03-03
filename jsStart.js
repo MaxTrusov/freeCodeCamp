3efbee1534c0fa95abdfca9f67bd3d9853d36f00
MaxTrusov <46316587+MaxTrusov@users.noreply.github.com>
2022-03-03 21:37:59 +0300

    objManipulating

--- jsStart.js
+++ jsStart.js
@@ -59,4 +59,48 @@ const testObj = {
 };
 
 const playerNumber = 16;
-const player = testObj[playerNumber];  
+const player = testObj[playerNumber];
+
+// Setup
+const myDog = {
+   "name": "Coder",
+   "legs": 4,
+   "tails": 1,
+   "friends": ["freeCodeCamp Campers"]
+};
+
+myDog.name = "Happy Coder";
+
+myDog.bark = "bow-wow";
+
+delete myDog.tails;
+
+// Setup
+function phoneticLookup(val) {
+   let result = "";
+
+   // Only change code below this line
+   const lookup = {
+      "alpha": "Adams",
+      "bravo": "Boston",
+      "charlie": "Chicago",
+      "delta": "Denver",
+      "echo": "Easy",
+      "foxtrot": "Frank"
+   }
+   result = lookup[val];
+
+   // Only change code above this line
+   return result;
+}
+phoneticLookup("charlie");
+ 
+// Manipulating Complex Objects
+function checkObj(obj, checkProp) {
+   if (obj.hasOwnProperty(checkProp)) {
+      return obj[checkProp];
+   } else {
+      return "Not Found";
+   }
+
+}
