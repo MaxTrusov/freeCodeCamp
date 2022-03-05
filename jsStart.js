523152250fa715b51893ee14fb92c1881ac386ae
MaxTrusov <46316587+MaxTrusov@users.noreply.github.com>
2022-03-05 20:12:10 +0300

    main

--- jsStart.js
+++ jsStart.js
@@ -339,3 +339,14 @@ function convertToInteger(str) {
    return parseInt(str);
 }
 convertToInteger("56");
+
+function convertToInteger(str) {
+   return parseInt(str, 2);
+}
+convertToInteger("10011");
+
+//
+function checkEqual(a, b) {
+   return a == b ? 'Equal' : 'Not Equal';
+}
+checkEqual(1, 2);
