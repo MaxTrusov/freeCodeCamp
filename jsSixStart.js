346695924535161c74b6a86c0910b99e6ab35c55
MaxTrusov <46316587+MaxTrusov@users.noreply.github.com>
2022-03-07 21:18:24 +0300

    Mutate an Array Declared with const

--- jsSixStart.js
+++ jsSixStart.js
@@ -20,3 +20,18 @@ function checkScope_1() {
    return i;
 }
 checkScope_1();
+
+
+
+//Mutate an Array Declared with const
+const s = [5, 7, 2];
+function editInPlace() {
+   // Only change code below this line
+   s[0] = 2;
+   s[1] = 5;
+   s[2] = 7;
+   // Using s = [2, 5, 7] would be invalid
+
+   // Only change code above this line
+}
+editInPlace();
