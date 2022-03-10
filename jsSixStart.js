0ae924234a23f567f630f239a55e0a4aeda1dfbc
MaxTrusov <46316587+MaxTrusov@users.noreply.github.com>
2022-03-10 15:50:40 +0300

    UseDest-g AssignmentToPass AnObjFunc'sParam

--- jsSixStart.js
+++ jsSixStart.js
@@ -125,11 +125,26 @@ const { today: { high: highToday_1 } } = LOCAL_FORECAST;
 let a = 8, b = 6;
 [a, b] = [b, a];
 
-
+//Use Destructuring Assignment with the Rest Parameter to Reassign Array Elements
 const source = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 function removeFirstTwo(list) {
 
-   const [a, b, ...arr] = list; 
+   const [a, b, ...arr] = list;
    return arr;
 }
 const arr = removeFirstTwo(source);
+
+
+//Use Destructuring Assignment to Pass an Object as a Function's Parameters
+const stats = {
+   max: 56.78,
+   standard_deviation: 4.34,
+   median: 34.54,
+   mode: 23.87,
+   min: -0.75,
+   average: 35.85
+};
+
+const half = ({ max, min }) => {
+   return (max + min) / 2;
+}  
