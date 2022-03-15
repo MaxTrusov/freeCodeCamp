fa8a9dd145b26fe8650846f98920b5d3acde1c7f
MaxTrusov <46316587+MaxTrusov@users.noreply.github.com>
2022-03-15 21:06:52 +0300

    Use getters and setters to Control Access to anObj

--- jsSixStart.js
+++ jsSixStart.js
@@ -201,3 +201,18 @@ class Vegetable {
 const carrot = new Vegetable('carrot');
 console.log(carrot.name);
 
+
+
+class Thermostat {
+   constructor(fahrenheit) {
+      this.fahrenheit = fahrenheit;
+   }
+   get temperature() {
+      return (5 / 9) * (this.fahrenheit - 32);
+   }
+   set temperature(celsius) {
+      this.fahrenheit = (celsius * 9.0) / 5 + 32;
+   }
+
+}
+
