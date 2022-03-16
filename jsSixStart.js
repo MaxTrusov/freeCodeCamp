5eff938891c893b318f64134fe3e485d585b0b64
MaxTrusov <46316587+MaxTrusov@users.noreply.github.com>
2022-03-16 18:31:12 +0300

    Use * to Import Everything from a File

--- jsSixStart.js
+++ jsSixStart.js
@@ -231,3 +231,10 @@ import {uppercaseString, lowercaseString}  from './index.js';
 
 uppercaseString("hello");
 lowercaseString("WORLD!");
+
+
+// Use * to Import Everything from a File
+import * as stringFunctions from "./string_functions.js";
+
+stringFunctions.uppercaseString("hello");
+stringFunctions.lowercaseString("WORLD!");
